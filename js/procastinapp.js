304e83496e3ec550c21f1b59198ec278c742b934
XabierGallardo <javiergallard91@gmail.com>
2020-04-27 18:21:28 +0200

    Added digital clock and minor design updates

--- js/procastinapp.js
+++ js/procastinapp.js
@@ -1,5 +1,29 @@
 //$(document).ready() shorcut
 $(function() {
+
+	//Start clock
+	setInterval(function(){
+		let d = new Date();
+                let hours = d.getHours();
+                let minutes = d.getMinutes();
+                let seconds = d.getSeconds();
+
+		if (hours < 10) {
+
+			hours = "0" + hours;
+
+		}else if (minutes < 10) {
+
+			minutes = "0" + minutes;
+
+		}else if (seconds < 10) {
+
+			seconds = "0" + seconds;
+		}
+
+                $("#options-results_clock").html(hours + ":" + minutes + ":"  +seconds);
+                }, 1000);
+
 	
 	//Show clock
 	$("#options-icons_clock").click(function() {
