be1e9acaa09619bb1845eb6cb5dce0ebe406e125
XabierGallardo <javiergallard91@gmail.com>
2020-05-28 03:55:00 +0200

    crono working, pausing and resetting. Must check all possible combinations to cheat functionality

--- js/procastinapp.js
+++ js/procastinapp.js
@@ -3,15 +3,18 @@ $(function() { //$(document).ready() shortcut
 	//INITIAL CONFIG////////////////////////////////////////////////////////////////////////////
 
 	//Set and display crono 
-	let cronoHours = 0;
-	let cronoMinutes = 0;
-	let cronoSeconds = 0;
-	let crono_start = false;
+	let startTime = 0;
+	let start = 0;
+	let end = 0;
+	let dif = 0;
+	let timerID = 0;
 
-	$("#crono-hours").html("0" + cronoHours);
-	$("#crono-minutes").html("0" + cronoMinutes);
-	$("#crono-seconds").html("0" + cronoSeconds);	
-
+	let cronoMSecs;
+	let cronoSeconds;
+	let cronoMinutes;
+	let cronoHours;
+	let crono_working = false;
+	let counter = 0;
 
 	//Set clock
 	setInterval(function() {
@@ -34,36 +37,35 @@ $(function() { //$(document).ready() shortcut
 			clockSeconds = "0" + clockSeconds;
 		}
 
-		//Print clock on the screem
+		//Print clock on the screen
 	    	$("#options-clock_results").html(clockHours + ":" + clockMinutes + ":"  + clockSeconds);
         
 	}, 1000);
 
 
+
+
+
 	//CRONO//////////////////////////////////////////////////////////////////////////////////////////
 	
-	/*To do: 
-	 * Reset button, fix values to 0
-	 * Don't start when the page is loaded
-	 * */
+        function crono () {
 
-        function chrono () {
-
-                let end = new Date();
-                let dif = end - start;
+                end = new Date();
+                dif = end - start;
 
                 dif = new Date(dif);
-		let cronoMSecs = dif.getMilliseconds();
-		let cronoSeconds = dif.getSeconds();
-		let cronoMinutes = dif.getMinutes();
-                let cronoHours = dif.getHours() - 1;
+
+		//cronoMSecs = dif.getMilliseconds();
+		cronoSeconds = dif.getSeconds();
+		cronoMinutes = dif.getMinutes();
+                cronoHours = dif.getHours() - 1;
 
 		 //Print values and add double digits
 		if (cronoSeconds < 10) {
 			cronoSeconds = "0" + cronoSeconds;
 		}
 
-		if(cronoMinutes < 10) {
+		if (cronoMinutes < 10) {
 			cronoMinutes = "0" + cronoMinutes;
 		}
 
@@ -71,85 +73,79 @@ $(function() { //$(document).ready() shortcut
 			cronoHours = "0" + cronoHours;
 		}
 
+		/* Optional Milliseconds
+		if (cronoMSecs < 10){
+			cronoMSecs = "00" + cronoMSecs;
+		}
 
+		if (cronoMSecs < 100) {
+			cronoMSecs = "0" + cronoMSecs;
+		}*/
 
+		$("#crono-hours").html(cronoHours);
+		$("#crono-minutes").html(cronoMinutes);
+		$("#crono-seconds").html(cronoSeconds);
+		//$("#crono-milliseconds").html(cronoMSecs);
+
+		timerID = setTimeout(crono, 1000);
 	}
 
-
 	$("#play-crono").click(function() {
-
-		//Reverse value and change icon
-		crono_start = !crono_start;
 		$("#play-crono").toggleClass("fa-pause fa-play");
+		crono_working = !crono_working;
+		console.log(crono_working);
+		counter++;
 
-		if (crono_start === true) { //While working
-			console.log(crono_start);
-
-		} else {
-			console.log(crono_start);
+		if (counter < 2) {
+			startCrono();
 		}
 	});
 
 
-	//Restart crono
-	//$("#restart-crono").click(restartCrono);
-
-
 	//Crono functions////////////////
-	/*function startCrono() {
+	function startCrono() {
+		if (crono_working === true) {
+			$(".fa-pause").click(stopCrono);
+		
+			$("#restart-crono").click(restartCrono);
 
-		setInterval(function () {
+			start = new Date();
 
-			if (cronoSeconds > 59) { //Add a minute each 60 seconds
-				cronoSeconds = 0;
-				cronoMinutes++;
-			}
-
-			if (cronoMinutes > 59) { //Add an hour each 60 minutes
-				cronoMinutes = 0;
-				cronoHours++;
-			}
-
-			if(cronoHours > 24) { //Add a max of 24 hours to reset values
-				cronoSeconds = 0;
-				cronoMinutes = 0;
-				cronoHours = 0;
-			}
-
-
-			//Print values and add double digits
-			if (cronoSeconds < 10) {
-				$("#crono-seconds").html("0" + cronoSeconds);
-			} else {
-				$("#crono-seconds").html(cronoSeconds);
-			}
-
-			if(cronoMinutes < 10) {
-				$("#crono-minutes").html("0" + cronoMinutes);	
-			} else {
-				$("#crono-minutes").html(cronoMinutes);
-			}	
-
-			if (cronoHours < 10) {
-				$("#crono-hours").html("0" + cronoHours);
-			} else {
-				$("#crono-hours").html(cronoHours);
-			}
-			cronoSeconds++;		
-		}, 1000);
+			crono();
+		}
 	}
 
+	function continueCrono() {
+		//if (crono_working === false) {
 
-	function restartCrono () {
-		crono_start = false;
-		$("#crono-hours").html("0" + cronoHours);
-	        $("#crono-minutes").html("0" + cronoMinutes);
-	        $("#crono-seconds").html("0" + cronoSeconds);
+		$(".fa-pause").click(stopCrono);
+		
+		$("#restart-crono").click(restartCrono);
 
-	}*/
+		start = new Date() - dif;
 
+		start = new Date(start);
 
+		crono();
+		//}
+	}
+
+	function restartCrono () { start = new Date();}
 	
+	function stopRestartCrono () { $(".fa-play").click(startCrono);}
+
+
+	function stopCrono() {
+		$(".fa-play").click(continueCrono);
+		$("#restart-crono").click(stopRestartCrono);
+
+		clearTimeout(timerID);
+	}
+	
+
+
+
+
 	//Show clock///////////////////////////////////////////////
 	$("#options-clock_icons").click(function() {
 		$("#options-clock_results").toggleClass("show");
